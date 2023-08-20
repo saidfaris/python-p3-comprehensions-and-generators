68838bef46f9f47615e91912111a1c699082bed6
sidee <said.issack@student.moringaschool.com>
2023-08-20 23:56:08 +0300

    completed assighnment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+arr = range(1,30)
 
 def return_evens(num_list):
     pass
+    return [num for num in num_list if num % 2 == 0 ]
 
 def make_exclamation(sentence_list):
     pass
+def make_exclamation(sentences):
+    exclamated_sentences = [sentence + "!" for sentence in sentences]
+    return exclamated_sentences
+
+
+test = return_evens(arr)
+print(test)
+
+sent = make_exclamation(['Hello', 'Put on this', 'Go away'])
+print(sent)
